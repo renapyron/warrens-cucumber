f134c8f1f20786276e1f641373f4f9dea3934440
Warren <ren_apyron@yahoo.com>
2024-04-26 00:10:09 +0800

    webdriver buggy

--- step-definitions/maths/readme-steps.ts
+++ step-definitions/maths/readme-steps.ts
@@ -1,20 +1,21 @@
 import { CustomWorld } from '../../world';
-import { SimpleMathsCalculator } from '../../domains/simple-maths/simple-maths-calculator';
 import { Before, Given, Then, When } from '@cucumber/cucumber';
 import { assert } from 'chai';
 
-let readmeUrl = '';
 // Ref: https://github.com/e2e-boilerplate/selenium-webdriver-typescript-ts-node-jasmine-expect/blob/master/spec/sandbox.spec.ts
 Given('Given a github readme url', async function (this: CustomWorld, value: number) {
   this.calculator.startWith(value);
 });
 
-When('When I load this page', async function (this: CustomWorld, value: number) {
-  this.calculator.incrementBy(value);
+When('When I load this page', async function (this: CustomWorld) {
+
+  await this.webdriver.get("https://github.com/renapyron/warrens-cucumber");
 });
 
-Then('it should contain the text "warren\'s cucumber"', async function (this: CustomWorld, value: number) {
-  assert.strictEqual(this.calculator.result, value);
+Then('it should contain the text "warren\'s cucumber"', async function (this: CustomWorld) {
+  const html = await this.webdriver.getPageSource();
+
+  assert.include(html, 'warren\'s cucumber');
 });
 
 /**
